420fe5b4707fd4b06b2c5afd460d9fa240902baa
Javier Robles <jrobles@despegar.com>
2021-08-29 19:18:46 -0300

    changes

--- src/main/java/com/despegar/jav/App.java
+++ src/main/java/com/despegar/jav/App.java
@@ -8,13 +8,22 @@ public class App {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
-    private static final String host = "dbData";
-    private static final int port = 65455;
+    /**
+     * host estable, bajo rate de errores
+     */
+    private static final String host = "dbUserDespegar.com";
+
+    /**
+     * Host inestable, alto rate de errores
+     */
+    //private static final String host = "dbUserOld.com";
+
+    private static final int port = 3015;
     private static final String user = "carlos.alvarez";
     private static final String pass = "despegar1234";
 
     public static void main(String[] args) {
-        final Connector connector = ConnectorDummy.getInstance(host, port, user, pass);
+        final Connector connector = ConnectorDummy.newInstance(host, port, user, pass);
         LOGGER.info("Connector built with host: {} and PORT: {}}", host, port); ;
         final Request request = new Request(1000);
 
@@ -30,7 +39,6 @@ public class App {
                     LOGGER.info("{} users scrolled", scrollResponse.read());
                     mustScroll = !scrollResponse.end();
                 }
-                LOGGER.debug("end while");
             } catch (SearchException se) {
                 LOGGER.error("Error while performing search operation", se);
             }
