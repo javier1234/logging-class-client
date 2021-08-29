1ccf81e14e9248d4c67291771a8b0537425881e6
Javier Robles <jrobles@despegar.com>
2021-08-29 19:24:05 -0300

    changes

--- src/main/java/com/despegar/jav/App.java
+++ src/main/java/com/despegar/jav/App.java
@@ -2,8 +2,22 @@ package com.despegar.jav;
 
 public class App {
 
+    /**
+     * host estable, bajo rate de errores
+     */
+    private static final String host = "dbUserDespegar.com";
+
+    /**
+     * Host inestable, alto rate de errores
+     */
+    //private static final String host = "dbUserOld.com";
+
+    private static final int port = 3015;
+    private static final String user = "carlos.alvarez";
+    private static final String pass = "despegar1234";
+
     public static void main(String[] args) {
-        final Connector connector = ConnectorDummy.getInstance("dbData", 65535, "carlos.alvarez", "despegar1234");
+        final Connector connector = ConnectorDummy.newInstance(host, port, "carlos.alvarez", "despegar1234");
         System.out.println("Connector built \n with user: carlos.alvaruez \n PORT: despegar1234");
         final Request request = new Request(1000);
 
@@ -11,9 +25,11 @@ public class App {
             Response searchResponse = connector.search(request);
             System.out.println("retrieved: " + searchResponse.toString() + "\n with request:" + request.toString());
             boolean mustScroll = !searchResponse.end();
+            int totalRead = 0;
             try {
                 while (mustScroll) {
                     Response scrollResponse = connector.scroll(request);
+                    totalRead += scrollResponse.read();
                     mustScroll = !scrollResponse.end();
                     System.out.println("retrieved: " + searchResponse.toString() + "\n with request:" + request.toString());
                 }
